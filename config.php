0bc2f8cf06415824430c132bae6af7111ca63ef5
Ivan Shiller <ivan.shiller@protonmail.com>
2023-03-14 00:03:41 +0100

    updated db for hosted version

--- config.php
+++ config.php
@@ -1,11 +1,19 @@
 <?php
 
 //adjust this to your needs
-$user = 'root';
-$password = 'root';
-$db = 'link_collector';
-$host = '127.0.0.1';
-$port = 3306;
+
+//local version (MAMP)
+// $user = 'root';
+// $password = 'root';
+// $db = 'link_collector';
+// $host = '127.0.0.1';
+// $port = 3306;
+
+//hosted version
+$user = 'u579090749_main_collector';
+$password = '$VQu-Wn$C:vTN#4X';
+$db = 'u579090749_link_collector';
+$host = 'localhost';
 
 //establish connection
 $con = mysqli_connect($host, $user, $password, $db);
